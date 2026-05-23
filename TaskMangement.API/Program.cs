3a5d4f9372ece462fe00a8458bd705e8c27c7ba7
Mohamed <mohamedshehatadev17@gmail.com>
2026-05-23 03:12:55 +0300

    add result pattern and establishing connection to database

--- TaskMangement.API/Program.cs
+++ TaskMangement.API/Program.cs
@@ -12,8 +12,8 @@ namespace TaskMangement.API
             // Add services to the container.
 
             builder.Services.AddControllers();
-            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
